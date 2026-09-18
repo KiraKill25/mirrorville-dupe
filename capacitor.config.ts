c52a06975870da83785d66f2019ae86bce414fb6
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-09-18 16:31:16 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,18 @@ const config: CapacitorConfig = {
   appId: "com.mouradsvilleomega",
   appName: "Mourad's Ville Omega",
   webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
   android: {
     backgroundColor: "#000000",
     allowMixedContent: false,
+  },
+  plugins: {
+    Keyboard: {
+      resize: "none",
+      resizeOnFullScreen: false,
+    },
   },
 };
 
